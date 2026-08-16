1d9b6df8c0315712126618b32358d9cdf52ff722
alMukaafih <almukaafih@gmail.com>
2026-08-16 07:29:12 +0100

    feat: completed step 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::process::{Command, exit};
+use std::ffi::CString;
+use std::process::exit;
 
 use clap::{Parser, Subcommand};
+use nix::errno::Errno;
+use nix::sys::signal::Signal;
+use nix::sys::wait::{WaitStatus, waitpid};
+use nix::unistd::{execvp, sethostname};
+
+use nix::sched::{CloneFlags, clone};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -12,17 +19,36 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Run the specified command
-    Run { command: String, args: Vec<String> },
+    Run {
+        command: CString,
+        args: Vec<CString>,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Run { command, args } => {
-            let mut child = Command::new(command).args(args).spawn()?;
-            let status = child.wait()?;
-            if let Some(code) = status.code() {
-                exit(code)
+        Commands::Run { command, mut args } => {
+            args.insert(0, command);
+            let mut stack = [0; 65536];
+
+            unsafe {
+                let pid = clone(
+                    Box::new(|| {
+                        sethostname("container").unwrap();
+                        let _ = execvp(&args[0], &args);
+
+                        Errno::last_raw() as _
+                    }),
+                    &mut stack,
+                    CloneFlags::CLONE_NEWUTS,
+                    Some(Signal::SIGCHLD as _),
+                )?;
+
+                match waitpid(pid, None)? {
+                    WaitStatus::Exited(_, code) => exit(code),
+                    _ => {}
+                }
             }
         }
     }
